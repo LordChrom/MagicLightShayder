1556c09a032a94f5b8912f173faa75bb39e844e4
LordChrom <slimer137@gmail.com>
2026-02-20 17:08:41 -0600

    colored the lights

--- shaders/lib/voxel/voxelMapper.glsl
+++ shaders/lib/voxel/voxelMapper.glsl
@@ -11,12 +11,23 @@
 
 
 
-void writeVoxelMap(vec3 worldPos, vec3 color, uint emission, bool translucent, bool opaque){
+void writeVoxelMap(vec3 worldPos, int blockID, uint emission, bool translucent, bool opaque){
+    vec3 color = vec3(0.9,0.6,0.6);
+    if(blockID>=0){
+        color.r=(blockID/100)%10;
+        color.g=(blockID/10)%10;
+        color.b=(blockID)%10;
+        color/=8;
+    }else{
+//        color=vec3(1,0.95,0.85);
+        color=vec3(1,0,0);
+
+    }
+//    color=normalize(color);
     ivec4 worldPosi = worldPosToSection(worldPos,1);
 
     if(!isVoxelInBounds(worldPos)) return;
 
     uint metadata = (emission<<4) + (uint(translucent)<<1) + uint(opaque);
     setVoxData(uvec4(255*color,metadata),worldPosi.xyz);
-
 }
--- shaders/shadow.vsh
+++ shaders/shadow.vsh
@@ -4,11 +4,13 @@ uniform mat4 shadowModelViewInverse;
 uniform vec3 cameraPosition;
 
 in vec4 at_midBlock;
+in vec2 mc_Entity;
 
 void main() {
     int emission = int(at_midBlock.w);
 
     vec3 worldPos = gl_Vertex.xyz-gl_ProjectionMatrix[3].xyz+cameraPosition;
     worldPos+= at_midBlock.xyz/64.0;
-    writeVoxelMap(worldPos,vec3(0.9,0.6,0.6),emission, false, emission==0);
+    int blockId = int(mc_Entity.x);
+    writeVoxelMap(worldPos,blockId,emission, false, emission==0);
 }
