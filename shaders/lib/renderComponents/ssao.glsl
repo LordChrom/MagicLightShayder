817587d080c545ed56f01e6d24131f271cf35b2d
LordChrom <slimer137@gmail.com>
2026-05-06 01:46:03 -0500

    Switched SSAO pattern from being random to being in a fixed pattern (big improvement, actually happy with it now).

--- shaders/lib/renderComponents/ssao.glsl
+++ shaders/lib/renderComponents/ssao.glsl
@@ -1,57 +1,46 @@
 #include "/lib/util/conversions.glsl"
 
-uint nextRand(uint value, uint seed){
-    value ^= value<<17;
-    value ^= value>>11;
-    value ^= seed ^ (seed>>5);
-    return value+seed;
-}
-
 
 float doSsao(vec2 texcoord, vec2 normalDir, float solidDepth, float dither){
-    dither = temporalNoise(dither);
-    uint seed = floatBitsToUint(dither) ^ (floatBitsToUint(dither + solidDepth)>>3);
-    seed ^= bitfieldReverse(seed);
-    uint rand = nextRand(seed,seed);
-
-    float goodSamples = 0;
+    float totalWeight = 0;
     float total = 0;
 
     solidDepth = depthToLinear(solidDepth);
     float radius = (SSAO_RADIUS*0.3)/solidDepth;
     radius = min(radius,0.15);
 
-    for(int i=0; i<SSAO_SAMPLES;i++){
-        rand = nextRand(rand,seed);
-        vec4 offset4 = unpackUnorm4x8(rand^(rand<<16));
-        vec2 offset = (offset4.xy-offset4.zw);
-        float len = length(offset);
-        if(len>1)
-            offset=offset*fract(len)/len;
 
-        float d = dot(normalize(offset),normalDir);
-        offset = d>-0?offset:-offset;
-        float validness = abs(d);
+    const int anglesPerLevel = 2;
+    for(int r = 1; r<=SSAO_QUALITY; r++){
+        float tempRadius = (r-dither)/(SSAO_QUALITY);
+        int angles = (anglesPerLevel*r)|1;
 
-        offset*=abs(offset)*radius;
-        offset+=texcoord;
-        if(offset.x>1.0 || offset.y>1.0 || offset.x<0 || offset.y<0)
-            continue;
-        float sampledDepth = texture(depthtex2,offset).x;
-        sampledDepth=depthToLinear(sampledDepth);
+        for(int a = 0; a<angles;a++){
+            float angle = (a+dither)*(6.28318530718/angles);
+            vec2 offset = vec2(cos(angle),sin(angle))*tempRadius;
+            float d = dot(normalize(offset),normalDir);
+            offset = d>-0?offset:-offset;
+            float weight = abs(d);
 
-        validness *= clamp((sampledDepth-solidDepth+0.8),0,1);
+            offset = offset*radius + texcoord;
 
-        goodSamples+=validness;
-        if(sampledDepth>=solidDepth)
-            total+=validness;
+            if(offset.x>1.0 || offset.y>1.0 || offset.x<0 || offset.y<0)
+                continue;
+            float sampledDepth = depthToLinear(texture(depthtex2,offset).x);
 
+            if(abs(sampledDepth-solidDepth)<0.001)
+                continue;
+
+            totalWeight+=weight;
+            if(sampledDepth>solidDepth)
+                total+=weight;
+        }
     }
-    if(goodSamples==0)
-        return 0.5;
-    float ssao = float(total)/float(goodSamples);
+
+    if(totalWeight==0)
+        return 1;
+    float ssao = float(total)/float(totalWeight);
     ssao*=ssao;
-    const float mult = (SSAO_STRENGTH/(pow(min(SSAO_SAMPLES,8),0.5)));
-    ssao=1-(1-ssao)*mult;
-    return clamp(ssao,0.3,1);
+    ssao=1-(1-ssao)*SSAO_STRENGTH;
+    return clamp(ssao,0.2,1);
 }
--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -36,9 +36,9 @@
 #define AREA_TRANSITION_DIST 0.85 //[-1 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.85 0.9 0.95]
 
 //#define SSAO
-#define SSAO_SAMPLES 6 //[1 2 3 4 6 8 12 16 24 32]
-#define SSAO_RADIUS 0.4 //[0.025 0.05 0.075 0.1 0.15 0.2 0.3 0.4 0.5 0.6 0.8 1.0 1.2 1.6 2.0]
-#define SSAO_STRENGTH 2.5 //[0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0 2.5 3.0 3.5 4.0 5.0 6.0 8.0 12.0 16.0]
+#define SSAO_QUALITY 3 //[1 2 3 4 6 8 10 16]
+#define SSAO_RADIUS 0.5 //[0.025 0.05 0.075 0.1 0.15 0.2 0.3 0.4 0.5 0.6 0.8 1.0 1.2 1.6 2.0]
+#define SSAO_STRENGTH 0.8 //[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.25 1.5 1.75 2.0]
 const float ambientOcclusionLevel = 1.0; //[0.0 0.25 0.5 0.75 1.0]
 
 #define VOX_LAYERS 2 //[1 2 3 4 5 6 7 8]
