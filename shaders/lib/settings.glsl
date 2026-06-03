be969a23a54122f2362687e3f8915b4892c349c5
LordChrom <slimer137@gmail.com>
2026-06-02 19:35:37 -0500

    Out with the old

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -114,7 +114,6 @@ const float ambientOcclusionLevel = 1.0; //[0.0 0.25 0.5 0.75 1.0]
 
 //-1 is none, 0 is shadow, 1 is culled vertex, 2 is unculled vertex
 #define VOXELIZATION_MODE 0 //[-1 0 1 2]
-#define NEW_OCCLUSION
 #define OCCLUDERS_PER_LIGHT 1 //[1 3 5]
 
 /////
