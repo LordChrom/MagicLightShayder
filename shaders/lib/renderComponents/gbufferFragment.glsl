c501a5d40e39f074f3894fa8e584a234228a9062
LordChrom <slimer137@gmail.com>
2026-06-07 03:37:48 -0500

    POM gets its own file, pom normals a thing

--- shaders/lib/renderComponents/gbufferFragment.glsl
+++ shaders/lib/renderComponents/gbufferFragment.glsl
@@ -111,90 +111,8 @@ in vec2 differential;
 flat in ivec2 baseTexpos;
 flat in ivec2 texsize;
 uniform ivec2 atlasSize;
-uniform float far, near;
-
-const float pomDepth = 0.25*POM_DEPTH_STRENGTH;
-
 float rayDepth=0;
-
-void pomEdge(inout vec2 tc){
-    #ifdef POM_DISCARD
-    if(tc.x<0 || tc.y<0 || tc.x>=texsize.x || tc.y>=texsize.y)
-        discard;
-    #endif
-
-    #ifdef POM_WRAP
-    tc= fract(tc/texsize)*texsize;
-    #else
-    tc= clamp(tc,vec2(0),texsize-1e-5);
-    #endif
-}
-
-vec2 doPixPom(vec2 initialTc){
-    const float tiny = exp2(-12);
-    vec2 tc = initialTc;
-    for(int i = 0; i<pomSamplesPix; i++){
-        rayDepth+=tiny;
-        tc =initialTc+differential*rayDepth;
-
-        pomEdge(tc);
-        vec2 remainingDepthTillTransition = (1-fract(tc*sign(differential)))/abs(differential);
-        rayDepth+=min(remainingDepthTillTransition.x,remainingDepthTillTransition.y);
-        float texdepth = float(1.0-texelFetch(normals,baseTexpos+ivec2(tc),0).a)*pomDepth;
-        if(rayDepth>= texdepth){
-            break;
-        }
-
-    }
-    return tc;
-}
-vec2 doSparsePom(vec2 initialTc){
-    vec2 tc;
-    for(int i = 0; i<pomSamplesSparse; i++){
-        rayDepth = i*(pomDepth/pomSamplesSparse);
-        tc =initialTc+differential*rayDepth;
-    #if POM_MODE==2
-        vec2 c = clamp(tc,vec2(0),texsize);
-        if(c!=tc){
-            tc=c;
-            break;
-        }
-    #else
-        pomEdge(tc);
-    #endif
-        float depth = float(1.0-texelFetch(normals,(baseTexpos+ivec2(tc)),0).a)*pomDepth;
-
-        if(rayDepth+1e-4>=depth){
-            break;
-        }
-    }
-    return tc;
-}
-
-vec2 doPom(vec2 tc){
-    vec2 initialTc = tc*atlasSize-baseTexpos;
-    vec2 ret;
-
-    #if POM_MODE==0
-    ret = doSparsePom(initialTc);
-    #elif POM_MODE==1
-    ret= doPixPom(initialTc);
-    #else
-    doSparsePom(initialTc);
-    float maxDepthDif = ((pomSamplesPix-2)/(ceil(abs(differential.x))+ceil(abs(differential.y))));
-    rayDepth=max(0,rayDepth-maxDepthDif);
-
-    ret= doPixPom(initialTc);
-    #endif
-
-    #ifdef POM_WRITE_DEPTH
-    rayDepth = float(1.0-texelFetch(normals,(baseTexpos+ivec2(ret)),0).a)*pomDepth;
-    #endif
-    return (baseTexpos+(0.5+floor(ret)))/atlasSize;
-
-}
-uniform mat4 gbufferProjectionInverse;
-#include "/lib/util/conversions.glsl"
+    #include "/lib/renderComponents/pom.glsl"
 #endif
 
 layout(location = 0) out vec4 color;
@@ -283,6 +201,13 @@ void main()
 
     pbrNormalSample.xy = (pbrNormalSample.xy-0.5)*(2*PBR_NORMALS_STRENGTH);
     vec3 texNormal = vec3(pbrNormalSample.xy,sqrt(1.0 - dot(pbrNormalSample.xy, pbrNormalSample.xy)));
+
+    #if (defined POM_NORMALS) && (defined POM)
+    vec3 pomEdgeNormal = vec3(
+        pomEdgeDif,0
+    );
+    texNormal = normalize(texNormal+1.5*pomEdgeNormal);
+    #endif
 
     texNormal = normalize(normalRotator*texNormal);
 
