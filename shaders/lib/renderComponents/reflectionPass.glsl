871fa8996c0f7f632befd8b4485cfea6055d6adf
LordChrom <slimer137@gmail.com>
2026-08-11 23:28:06 -0500

    Improved reflections more, multiple bounces, better handling of various edge cases, better cutoffs near screen edge, better prevention of streaks, etc

--- shaders/lib/renderComponents/reflectionPass.glsl
+++ shaders/lib/renderComponents/reflectionPass.glsl
@@ -53,25 +53,44 @@ vec3 debugChecker(vec3 value){
     return vec3(abs(value)*vec3(checker.x>=0,checker.y>=0,checker.z>=0));
 }
 
+const int stepsPerBounce=REFLECTION_QUALITY/REFLECTION_BOUNCES;
 
-vec3 doMarch(vec3 initialPos, vec3 viewDir){
+vec3 doMarch(vec3 initialPos, vec3 viewDir, out uint hitReason){
     vec2 differential = viewDir.xy/viewDir.z;
-    const int steps=REFLECTION_QUALITY;
     vec2 remainingScreen = vec2(differential.x>0?1-initialPos.x:initialPos.x,differential.y>0?1-initialPos.y:initialPos.y);
     float stepSize=min(remainingScreen.x/abs(differential.x),remainingScreen.y/abs(differential.y))/REFLECTION_QUALITY;
-    for(int i=0;i<=steps;i++){
+    #if REFLECTION_BOUNCES>1
+    stepSize*=min(2,1+0.3*REFLECTION_BOUNCES);
+    #endif
+    bool seenAir = false;
+    for(int i=0;i<=stepsPerBounce;i++){
         float depthDist = stepSize*(i+noise)+0.001;
         vec3 newPos = initialPos+vec3(depthDist*differential,depthDist);
-        if(newPos.x<0||newPos.y<0||newPos.x>1||newPos.y>1)
-            return vec3(-1);
-        float texDepth = texture(depthtex2,newPos.xy).x;
-        if(texDepth<=newPos.z)
+        float distFromEdge =min(min(newPos.x,newPos.y),1-max(newPos.x,newPos.y));
+        if(distFromEdge<noise*0.1){
+            hitReason=1;
             return newPos;
+        }
+        float texDepth = texture(depthtex2,newPos.xy).x;
+        if(texDepth<=newPos.z){
+            hitReason=0;
+            float linearTargetDepth = depthToLinear(newPos.z);
+            float depthDif = (linearTargetDepth-depthToLinear(texDepth))/linearTargetDepth;
+            if(depthDif>0.1)
+                hitReason=1;
+            if(seenAir){
+                return newPos;
+            }
+        }else{
+            seenAir=true;
+        }
     }
 
-    return vec3(-1);
+    hitReason=2;
+    return initialPos+stepSize*stepsPerBounce*vec3(differential,1);
 }
 
+#define REFLECTION_THRESHOLD 0.05
 void main() {
     vec3 screenPos;
     screenPos.xy=texcoord;
@@ -82,8 +101,8 @@ void main() {
     outputColor = texture(colortex0,texcoord,0).rgb;
     vec3 reflectionMult = vec3(1);
 
-    #define BOUNCES 1
-    for(int i=0;i<BOUNCES;i++)
+    bool continuing = false;
+    for(int i=0;i<REFLECTION_BOUNCES;i++)
     {
         uint reflectance = texture(colortex3,screenPos.xy).g;
         vec3 albedo = texture(colortex4,screenPos.xy).rgb;
@@ -96,19 +115,22 @@ void main() {
             reflectionMult*=albedo;
         }
         #endif
-        if(reflectance<=2)
+        if(reflectionMult.r+reflectionMult.g+reflectionMult.b<REFLECTION_THRESHOLD)
             return;
 
+        if(!continuing)
         worldDir = reflect(worldDir,normal);
         vec3 viewDir=worldDirToScreen(worldDir,screenPos);
         if(viewDir.z<0)
             return;
 
-        screenPos = doMarch(screenPos,viewDir);
-
-        if(screenPos==vec3(-1))
+        uint rayHitReason;
+        screenPos = doMarch(screenPos,viewDir,rayHitReason);
+        continuing=rayHitReason==2;
+        if(rayHitReason==1)
             return;
-
-        outputColor+= texture(colortex0,screenPos.xy).rgb*reflectionMult;
+        if(rayHitReason==0){
+            outputColor+= texture(colortex0,screenPos.xy).rgb*reflectionMult;
+        }
     }
 }
