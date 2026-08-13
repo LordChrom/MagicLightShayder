527d0e837c5a743404edf5a2cd350d9c0bf44b7b
LordChrom <slimer137@gmail.com>
2026-08-13 02:13:01 -0500

    better antistreaking & hand rejection, reorganized main loop a bit

--- shaders/lib/renderComponents/reflectionPass.glsl
+++ shaders/lib/renderComponents/reflectionPass.glsl
@@ -18,7 +18,7 @@ uniform usampler2D colortex8;
 /* RENDERTARGETS: 0 */
 layout(location = 0) out vec3 outputColor;
 
-vec3 reflect(vec3 dir, vec3 norm){
+vec3 vectorReflect(vec3 dir, vec3 norm){
     return normalize(dir-norm*(2*dot(norm,dir)));
 }
 
@@ -27,8 +27,6 @@ vec3 worldDirToScreen(vec3 worldDirection, vec3 screenPos){
 
     return normalize(pos.xyz-pos.w*(2*screenPos-1));
 }
-
-const int stepsPerBounce=REFLECTION_QUALITY/REFLECTION_BOUNCES;
 
 vec3 doMarch(vec3 initialPos, vec3 viewDir, float ditherValue, out uint hitReason){
     hitReason=2;
@@ -39,10 +37,11 @@ vec3 doMarch(vec3 initialPos, vec3 viewDir, float ditherValue, out uint hitReaso
     vec3 newPos;
     float texDepth;
 
+    const int stepsPerBounce=REFLECTION_QUALITY/REFLECTION_BOUNCES;
     for(int i=0;i<stepsPerBounce;i++){
         newPos = initialPos+(i+ditherValue)*viewDir;
         float distFromEdge =min(min(newPos.x,newPos.y),1-max(newPos.x,newPos.y));
-        if(distFromEdge<ditherValue*0.0 || newPos.z>=0.9999){
+        if(distFromEdge<ditherValue*0.05){
             hitReason=1;
             break;
         }
@@ -52,18 +51,19 @@ vec3 doMarch(vec3 initialPos, vec3 viewDir, float ditherValue, out uint hitReaso
         if(texDepth<=newPos.z){
             hitReason=0;
             break;
-
         }
     }
 
-    const float depthLeniency = clamp(2.0/REFLECTION_QUALITY,0.05,0.8);
+    const float depthLeniency = 4.0/REFLECTION_QUALITY;
 
     if(hitReason==0){
         texDepth=depthToLinear(texDepth);
         float targetDepth = depthToLinear(newPos.z);
-        float depthDif = abs(texDepth-targetDepth)/targetDepth;
-        if(depthDif>depthLeniency)
+        float depthDif = (texDepth-targetDepth);
+        depthDif*=depthDif/targetDepth;
+        if(depthDif>depthLeniency){
             hitReason=1;
+        }
     }
 
     return newPos;
@@ -76,55 +76,75 @@ vec3 doMarch(vec3 initialPos, vec3 viewDir, float ditherValue, out uint hitReaso
 void main() {
     vec3 screenPos;
     screenPos.xy=texcoord;
+
+    vec4 normal = texture(colortex2,texcoord);
+    outputColor = texture(colortex0,texcoord,0).rgb;
+
     screenPos.z = texture(depthtex0,texcoord).x;
     vec3 worldDir = normalize(worldDirNormalizeMe);
 
     float ditherValue = dither(ivec2(gl_FragCoord.xy));
-    outputColor = texture(colortex0,texcoord,0).rgb;
     vec3 reflectionMult = vec3(1);
 
-    bool continuing = false;
+
+    if(abs(normal.a-0.5)<0.1)
+        return;
+    normal.xyz=normalize(normal.xyz*2-1);
+
     vec3 viewDir;
-    #if REFLECTION_BOUNCES!=1
+
+    #if REFLECTION_BOUNCES>1
+    bool continuing = false;
     for(int i=0;i<REFLECTION_BOUNCES;i++)
     #endif
     {
-        if(!continuing){
+        #if REFLECTION_BOUNCES>1
+        if(!continuing)
+        #endif
+        {
+
             float transAlpha = texture(colortex3,screenPos.xy).a;
             uint reflectance = texture(colortex8,screenPos.xy).g;
             vec3 albedo = texture(colortex1,screenPos.xy).rgb;
-            vec4 normal = texture(colortex2,screenPos.xy);
+
+            worldDir = vectorReflect(worldDir, normal.xyz);
+            viewDir=worldDirToScreen(worldDir, screenPos);
+
             #ifndef PERFECT_MIRRORS
-            if(transAlpha>0.05){
-                reflectance=229;
-            }
-            if(abs(normal.a-0.5)<0.1 || transAlpha==1)
+            if(transAlpha==1){
                 return;
-            if(reflectance<=229){
+            }else if(transAlpha>0.05){
+                reflectionMult*=1-dot(worldDir,normal.xyz);
+            }else if(reflectance<=229){
                 reflectionMult*=(reflectance/255.0);
             }else{
                 //TODO hardcoded metals
                 reflectionMult*=albedo;
             }
-            #endif
+
             if(reflectionMult.r+reflectionMult.g+reflectionMult.b<REFLECTION_THRESHOLD)
                 return;
-            normal.xyz=normalize(normal.rgb*2-1);
-            worldDir = reflect(worldDir, normal.xyz);
-            viewDir=worldDirToScreen(worldDir, screenPos);
+            #endif
         }
 
         uint rayHitReason;
 
         screenPos = doMarch(screenPos,viewDir,ditherValue,rayHitReason);
 
+        #if REFLECTION_BOUNCES>1
         continuing=rayHitReason==2;
+        #endif
         if(rayHitReason==1)
             return;
-        vec3 normal = normalize(texture(colortex2,screenPos.xy).xyz*2-1);
+        if(rayHitReason==0){
+            normal = texture(colortex2,screenPos.xy);
+            normal.xyz=normalize(normal.xyz*2-1);
 
-        if(rayHitReason==0 && dot(worldDir,normal)<0){
-            outputColor+= texture(colortex0,screenPos.xy).rgb*reflectionMult;
+            if(abs(normal.a-0.5)<0.1)
+                return;
+
+            if(dot(worldDir,normal.xyz)<0)
+                outputColor+= texture(colortex0,screenPos.xy).rgb*reflectionMult;
         }
     }
 }
