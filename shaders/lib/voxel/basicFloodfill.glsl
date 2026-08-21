638a1d8ab08356990236aa1115a705989e827c92
LordChrom <slimer137@gmail.com>
2026-08-21 15:07:45 -0500

    distance based floodfill calculation  speed

--- shaders/lib/voxel/basicFloodfill.glsl
+++ shaders/lib/voxel/basicFloodfill.glsl
@@ -8,10 +8,25 @@
 #define WRITES_FLOOD
 #define SAMPLES_VOX
 #include "/lib/voxel/voxelHelper.glsl"
+#include "/lib/util/dither.glsl"
 
 
-const ivec3 workGroups = ivec3(1,FLOODFILL_SIZE,FLOODFILL_SIZE);
-layout (local_size_x = FLOODFILL_SIZE, local_size_y = 1, local_size_z = 1) in;
+#if FLOODFILL_SIZE<=32
+    #define WORK_SIZE 2
+#elif FLOODFILL_SIZE<=64
+    #define WORK_SIZE 4
+#elif FLOODFILL_SIZE<=128
+    #define WORK_SIZE 8
+#elif FLOODFILL_SIZE<=192
+    #define WORK_SIZE 12
+#elif FLOODFILL_SIZE<=256
+    #define WORK_SIZE 16
+#else
+    #define WORK_SIZE 24
+#endif
+
+const ivec3 workGroups = ivec3(WORK_SIZE,WORK_SIZE,WORK_SIZE);
+layout (local_size_x = 16, local_size_y = 1, local_size_z = 16) in;
 
 const float oneLightLevel = 1.0/15.0;
 const float oneStep = 1.0/255.0;
@@ -56,63 +71,87 @@ void considerSample(ivec3 offset){
     lightOutput=max(lightOutput,sampleLight);
 }
 
+uint bayer2u3d(uvec3 pos){
+    return ((pos.x&1u)<<2)+((pos.y&1u)<<1)+(pos.z&1u);
+}
+
+uint bayer4u3d(uvec3 pos){
+    return (bayer2u3d(pos)<<3)|(bayer2u3d(pos>>1));
+}
+
 void main(){
-    localPos = ivec3(gl_LocalInvocationID.x,gl_WorkGroupID.yz);
-    lightOutput=vec4(0);
+    //TODO seam filling, probably shared mem also
+    #define DISTANCE_BASED_FLOODFILL_SPEED
+    #ifdef DISTANCE_BASED_FLOODFILL_SPEED
+    ivec3 centerness = ivec3(gl_WorkGroupID)-(WORK_SIZE>>1)+1;
+    centerness=abs(centerness-clamp(centerness,0,1));
+    uint distFromCenter = max(max(centerness.x,centerness.y),centerness.z);
+
+    uint updatePeriod = max(distFromCenter*distFromCenter,1);
+    bool shouldCompute = (((bayer4u3d(gl_WorkGroupID)+frameCounter)%updatePeriod)==0);
+    if(!shouldCompute)
+        return;
+    #endif
+
     floodShift=getFloodShift();
 
+    for(int i=0;i<16;i++){
+        localPos = ivec3(gl_LocalInvocationID+(gl_WorkGroupID<<4));
+        localPos.y+=15-i;
+        lightOutput=vec4(0);
 
-    //TODO make unit scale voxelization a real thing
-    #if (FLOODFILL_SIZE>AREA_SIZE)
-    ivec3 areaPos;
-    ivec3 areaShift;
-    uint areaMemOffset;
-    for(uint cascade = 0; cascade<NUM_CASCADES;cascade++){
-        int scale=int(getScale(cascade));
-        if(scale<1)
-            continue;
-        areaShift = getAreaShift(scale);
-        areaPos = ((localPos-(FLOODFILL_SIZE>>1)+(floodShift&(scale-1)))/scale)+(AREA_SIZE>>1);
-        int minCoord = min(min(areaPos.x,areaPos.y),areaPos.z);
-        int maxCoord = max(max(areaPos.x,areaPos.y),areaPos.z);
-        areaMemOffset = areaOffset(cascade);
-        if((minCoord>=0) && (maxCoord<AREA_SIZE))
-            break;
-    }
-    #else
-        #define areaPos localPos
-        #define areaShift floodShift
-        #define areaMemOffset 0
-    #endif
-
-
-    currentBlock = getVoxData(areaPos, areaShift, areaMemOffset);
-    #ifdef OBSTRUCTION_MAPPING
-    uint obstruction = getObstructionData(areaPos, areaShift, areaMemOffset);
-    #endif
-
-
-    if(!bool(currentBlock&WORLDVOX_OPAQUE))
-    {
-        for(uint i=0;i<6;i++){
-            uint axis = i>>1;
-            ivec3 offset = ivec3(axis==0,axis==1,axis==2)*(bool(i&1u)?1:-1);
-
-            #ifdef OBSTRUCTION_MAPPING
-            if(!bool(obstruction&(1u<<i)))
-            #endif
-                considerSample(offset);
+        //TODO make unit scale voxelization a real thing
+        #if (FLOODFILL_SIZE>AREA_SIZE)
+        ivec3 areaPos;
+        ivec3 areaShift;
+        uint areaMemOffset;
+        for(uint cascade = 0; cascade<NUM_CASCADES;cascade++){
+            int scale=int(getScale(cascade));
+            if(scale<1)
+                continue;
+            areaShift = getAreaShift(scale);
+            areaPos = ((localPos-(FLOODFILL_SIZE>>1)+(floodShift&(scale-1)))/scale)+(AREA_SIZE>>1);
+            int minCoord = min(min(areaPos.x,areaPos.y),areaPos.z);
+            int maxCoord = max(max(areaPos.x,areaPos.y),areaPos.z);
+            areaMemOffset = areaOffset(cascade);
+            if((minCoord>=0) && (maxCoord<AREA_SIZE))
+                break;
         }
-        lightOutput= decayBlocklight(lightOutput);
-    }
+        #else
+            #define areaPos localPos
+            #define areaShift floodShift
+            #define areaMemOffset 0
+        #endif
 
-    vec3 blockColor = worldVoxColor(currentBlock);
-    if (bool(currentBlock&(0xfu<<WORLDVOX_TYPE_SHIFT))){
-        lightOutput.rgb=max(lightOutput.rgb,blockColor);
-    }else if(bool(currentBlock&WORLDVOX_TRANSLUCENT)){
-        lightOutput.rgb*=normalize(blockColor);
-    }
 
-    setFloodData(lightOutput, localPos, floodShift);
+        currentBlock = getVoxData(areaPos, areaShift, areaMemOffset);
+        #ifdef OBSTRUCTION_MAPPING
+        uint obstruction = getObstructionData(areaPos, areaShift, areaMemOffset);
+        #endif
+
+
+        if(!bool(currentBlock&WORLDVOX_OPAQUE))
+        {
+            for(uint i=0;i<6;i++){
+                uint axis = i>>1;
+                ivec3 offset = ivec3(axis==0,axis==1,axis==2)*(bool(i&1u)?1:-1);
+
+                #ifdef OBSTRUCTION_MAPPING
+                if(!bool(obstruction&(1u<<i)))
+                #endif
+                    considerSample(offset);
+            }
+            lightOutput= decayBlocklight(lightOutput);
+        }
+
+        vec3 blockColor = worldVoxColor(currentBlock);
+        if (bool(currentBlock&(0xfu<<WORLDVOX_TYPE_SHIFT))){
+            lightOutput.rgb=max(lightOutput.rgb,blockColor);
+        }else if(bool(currentBlock&WORLDVOX_TRANSLUCENT)){
+            lightOutput.rgb*=normalize(blockColor);
+        }
+
+        setFloodData(lightOutput, localPos, floodShift);
+    }
 }
 
