993d84fa26a038c5c6e59bcbb6ade9b2d79efd45
LordChrom <slimer137@gmail.com>
2026-09-10 17:10:13 -0500

    revisited debug gizmo

--- shaders/lib/renderComponents/bonusHudElements.glsl
+++ shaders/lib/renderComponents/bonusHudElements.glsl
@@ -4,7 +4,10 @@ uniform mat4 gbufferModelView;
 
 const vec2 bonusHudSpot = vec2(0.03,0.03);
 const float bonusHudScale = 0.02;
-const float bonusHudBirghtness = 0.25;
+const float bonusHudBirghtness = 2;
+const float bonusHudOpacity = 1;
+const float bonusHudWidth = 0.12;
+const float bonusHudBorderThickness = 0.2;
 
 
 void renderAxisGizmo(inout vec3 color, vec2 texcoord){
@@ -21,29 +24,60 @@ void renderAxisGizmo(inout vec3 color, vec2 texcoord){
 
     float len = length(texcoord);
     if(len>1) return;
-    if(0.95<len) color+=5.0/255;
+    vec3 hudColor = vec3(0);
+
+
 
     float angle = atan(texcoord.y,texcoord.x);
-    texcoord=normalize(texcoord);
+
+
+
     vec3 axisAngles=vec3(
         atan(gbufferModelView[0][1],gbufferModelView[0][0]),
         PI/2,
         atan(gbufferModelView[2][1],gbufferModelView[2][0])
     );
-    axisAngles=abs(axisAngles-angle);
-    float wideness = atan(0.07/len);
 
-    vec3 axisLen = abs(vec3(
+    axisAngles=abs(axisAngles-angle);
+    axisAngles=min(axisAngles,2*PI-axisAngles);
+
+
+    vec3 axisLen = vec3(
         gbufferModelView[0].z,
         gbufferModelView[1].z,
         gbufferModelView[2].z
-    ));
-    axisLen=pow(1-axisLen,vec3(0.25)); //probably not correct but its pretty close & i care about perf more than the remaining difference
+    );
 
-    if((axisAngles.x<=wideness || axisAngles.x>=2*PI-wideness) && (len<=axisLen.x))
-        color.r+=bonusHudBirghtness;
-    if((axisAngles.y<=wideness || axisAngles.y>=2*PI-wideness) && (len<=axisLen.y))
-        color.g+=bonusHudBirghtness;
-    if((axisAngles.z<=wideness || axisAngles.z>=2*PI-wideness) && (len<=axisLen.z))
-        color.b+=bonusHudBirghtness;
+    if(len>1-bonusHudBorderThickness){
+        hudColor=(asin(abs(axisLen))-0.5*axisAngles)/PI;
+        hudColor=step(-hudColor,vec3(0)) * max(step(fract(hudColor*12+0.25),vec3(0.5))*0.7+0.3,step(axisLen,vec3(0)));
+        hudColor*=abs(axisLen);
+
+    }else {
+        len/=1-(bonusHudBorderThickness+0.1);
+
+
+        axisAngles/= atan(bonusHudWidth/len);
+
+        axisLen=pow(1-abs(axisLen), vec3(0.25));//probably not correct but its pretty close & i care about perf more than the remaining difference
+
+        hudColor+=bonusHudBirghtness*clamp((1-axisAngles)*3, 0, 1)*clamp((axisLen-len)*100, 0, 1);
+
+        if (len<bonusHudWidth)
+            hudColor=vec3(bonusHudBirghtness);
+
+        axisLen = vec3(
+            gbufferModelView[0].z,
+            gbufferModelView[1].z,
+            gbufferModelView[2].z
+        );
+
+        if(hudColor.r>0 && axisLen.r>max(axisLen.g,axisLen.b))
+            hudColor.gb=vec2(0);
+        if(hudColor.g>0 && axisLen.g>max(axisLen.r,axisLen.b))
+            hudColor.rb=vec2(0);
+        if(hudColor.b>0 && axisLen.b>max(axisLen.r,axisLen.g))
+            hudColor.rg=vec2(0);
+    }
+    color = mix(color,hudColor,bonusHudOpacity);
 }
